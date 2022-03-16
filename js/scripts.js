ca30c04357b88e8656de57da1b8eb86dcde32693
Nicolas vauché <hello@nicolasvauche.net>
2022-03-16 19:01:47 +0100

    Form validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,8 @@
-// On importe notre fonction depuis un module
-import { validateFormInput } from './modules/forms.js'
+// On importe des fonctions depuis des modules
+import { validateForm } from './modules/forms.js'
 
 // On pointe sur le formulaire de contact
 const contactForm = document.getElementById('contactform')
 
-contactForm.addEventListener('submit', event => {
-  event.preventDefault()
-
-  for (const formInput of contactForm.elements) {
-    console.log(validateFormInput(formInput))
-  }
-})
+// On valide le formulaire de contact
+validateForm(contactForm)
